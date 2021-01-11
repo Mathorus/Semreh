e46e6035f9b541f6bd3f4140c49c6838f9a4ca1d
Matheus Borges <matheus.conquer@gmail.com>
2021-01-10 23:32:50 -0300

    ATUALIZAÇÃO 10/01
    PARTE DO SENSOR LATERAL PINAGEM E COMEÇO DA PARADA

---
+++ esse_aqui/src/setup.h
@@ -0,0 +1,35 @@
+// portas do esp
+
+//Sensores 
+#define numero_sensores 8;
+
+//portas
+//array
+#define sensores_array 13;  
+//laterais
+#define sensor_lateral_esquerdo 35;
+#define indicador_lateral_esquerdo 1;
+#define sensor_lateral_direito 34;
+#define indicador_lateral_direito 1;
+//drive
+#define pwm_direito_a 4;
+#define ain1 21;
+#define ain2 5;
+
+#define pwm_esquerdo_b 2;
+#define b1n1 18;
+#define b1n2 19;
+
+//encoder
+
+#define encoder_motor_dir_a 17;
+#define encoder_motor_dir_b 16;
+#define encoder_motor_esq_a 36;
+#define encoder_motor_esq_b 39;
+
+
+//pista
+
+#define qtde_curvas 1;
+#define curvas_esquerda 1; //??????????????
+#define distancia da pista 1;
